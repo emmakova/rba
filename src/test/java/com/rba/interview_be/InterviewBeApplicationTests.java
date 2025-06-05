65cb4854438163f7ae673b886ab7a4a8c6e64b23
Emil Makovac <emil.makovac@gmail.com>
2025-06-05 14:34:21 +0200

    Initial commit

---
+++ src/test/java/com/rba/interview_be/InterviewBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rba.interview_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
