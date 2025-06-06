234a469699c352ea827230f407b0b263145ca779
Emil Makovac <emil.makovac@gmail.com>
2025-06-06 12:54:26 +0200

    generate NewRequestApiClient with openapi generator

---
+++ src/main/java/com/rba/interview_be/config/BeanConfig.java
@@ -0,0 +1,15 @@
+package com.rba.interview_be.config;
+
+import com.rba.interview_be.service.NewCardRequestApiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public NewCardRequestApiClient newCardRequestApiClient(@Value("${new-card-request-api.base-url}") String baseUrl){
+        return new NewCardRequestApiClient(baseUrl);
+    }
+}
---
+++ src/main/java/com/rba/interview_be/service/NewCardRequestApiClient.java
@@ -0,0 +1,40 @@
+package com.rba.interview_be.service;
+
+import com.example.api.NewCardRequestApi;
+import com.example.invoker.ApiClient;
+import com.example.model.NewCardRequest;
+import com.rba.interview_be.entities.UserCardStatusEntity;
+import com.rba.interview_be.entities.UserEntity;
+import com.rba.interview_be.enums.CardStatusEnum;
+
+import java.util.Optional;
+
+import static com.rba.interview_be.utils.UserUtils.extractLastCardStatus;
+
+
+public class NewCardRequestApiClient {
+
+    private final NewCardRequestApi newCardRequestApi;
+
+    public NewCardRequestApiClient(String baseUrl) {
+        ApiClient client = new ApiClient();
+        client.setBasePath(baseUrl);
+        this.newCardRequestApi = new NewCardRequestApi(client);
+    }
+
+
+    public void submitNewCardRequestForUser(UserEntity userEntity) {
+
+        NewCardRequest newCardRequest = new NewCardRequest();
+        newCardRequest
+                .firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .oib(userEntity.getOib())
+                .status(Optional.ofNullable(extractLastCardStatus(userEntity.getCardStatuses()))
+                        .map(UserCardStatusEntity::getStatus)
+                        .map(CardStatusEnum::name)
+                        .orElse(null));
+
+        newCardRequestApi.apiV1CardRequestPost(newCardRequest);
+    }
+}
