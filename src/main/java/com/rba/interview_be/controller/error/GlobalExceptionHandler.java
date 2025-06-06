e864ff18c8d5876a91dc2d1479cc8234633b5554
Emil Makovac <emil.makovac@gmail.com>
2025-06-06 17:19:22 +0200

    add logging

--- src/main/java/com/rba/interview_be/controller/error/GlobalExceptionHandler.java
+++ src/main/java/com/rba/interview_be/controller/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.rba.interview_be.controller.error;
 
 import com.rba.interview_be.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,6 +20,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleGeneric(Exception ex) {
+        log.error(ex.getMessage(), ex);
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred");
     }
 
