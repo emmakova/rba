8d940139c67d7a70fc9eabf1bba30780e2143731
Emil Makovac <emil.makovac@gmail.com>
2025-06-09 19:53:46 +0200

    Fix integration test contextLoads()

--- src/test/java/com/rba/interview_be/InterviewBeApplicationTests.java
+++ src/test/java/com/rba/interview_be/InterviewBeApplicationTests.java
@@ -2,8 +2,10 @@ package com.rba.interview_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 class InterviewBeApplicationTests {
 
 	@Test
