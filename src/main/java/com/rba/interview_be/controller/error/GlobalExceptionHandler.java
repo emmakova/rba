47c785ba93bdb547f07a1d422cc10a92ac5863b3
Emil Makovac <emil.makovac@gmail.com>
2025-06-09 16:38:04 +0200

    Add logging in GlobalExceptionHandler

--- src/main/java/com/rba/interview_be/controller/error/GlobalExceptionHandler.java
+++ src/main/java/com/rba/interview_be/controller/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.rba.interview_be.controller.error;
 
 import com.rba.interview_be.exceptions.NotFoundException;
 import com.rba.interview_be.exceptions.StatusUpdateException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,21 +10,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorDto> handleResourceNotFound(NotFoundException ex) {
+        log.error(ex.getMessage(), ex);
         return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(StatusUpdateException.class)
     public ResponseEntity<ErrorDto> handleStatusUpdateException(StatusUpdateException ex) {
+        log.error(ex.getMessage(), ex);
         return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleGeneric(Exception ex) {
+        log.error(ex.getMessage(), ex);
         return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred");
     }
 
