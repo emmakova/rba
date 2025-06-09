b69b5d5e87c18f5c0458cc1117fb5cd331fe5d49
Emil Makovac <emil.makovac@gmail.com>
2025-06-09 18:08:57 +0200

    always clean and build image when running this script

--- dev-up.sh
+++ dev-up.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-if [[ "$(docker images -q rba-app:latest 2> /dev/null)" == "" ]]; then
-  echo "Building Docker image..."
-  ./gradlew clean jibDockerBuild
-else
-  echo "Image already exists, skipping Jib build"
-fi
+echo "Building Docker image..."
+./gradlew clean jibDockerBuild
 
 echo "Starting all services with docker-compose..."
 docker-compose up -d
